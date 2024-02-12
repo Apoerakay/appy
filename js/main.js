24a7e9bfaea80ba9bbd045cb7ed5295519ec9620
Kevin <apoerak@gmail.com>
2024-02-12 02:01:39 -0800

    work on appy navbar

---
+++ js/main.js
@@ -0,0 +1,18 @@
+ var firstName = "Kevin";  // first name is a word
+ var lastName = "Apoera";
+ var age = 16// age is a number
+//  let fullName = firstName + lastName;
+let fullName = `${firstName} ${lastName}`;
+ console.log(fullName);
+
+ const boilingWaterTemperature = 100;
+
+ let areYouHappy = true;
+
+ let myPhone = {color:"black",makeACall:null}   
+
+
+ if(age >=18) {console.log("welcome to KP Photography")}
+ else{console.log("this page is not avalaible")}
+
+ 
